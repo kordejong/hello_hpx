9f52d2275cbf9d1ca3187b97ce9f79fb1f0767a4
Kor de Jong <k.dejong1@uu.nl>
2019-11-20 11:59:17 +0100

    Add debug build

---
+++ debug_build.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -e
+set -x
+
+build_type=Debug
+variant=default
+
+
+basename=hello_hpx
+repository_cache=$HOME/development/repository  # Optional
+
+source_root="$(cd "$(dirname "$BASH_SOURCE")"; pwd -P)"
+tmp_root=${TMPDIR:-/tmp}/$basename/$build_type-$variant
+build_root=$tmp_root/build
+
+
+mkdir -p `dirname $build_root`
+mkdir $build_root
+cmake \
+    -DCMAKE_BUILD_TYPE=$build_type \
+    -DHELLO_HPX_REPOSITORY_CACHE:PATH=$repository_cache \
+    -S $source_root \
+    -B $build_root
+# echo cmake --build $build_root

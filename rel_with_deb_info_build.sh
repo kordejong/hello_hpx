19b2fde720cb6738793869e5b1076237cce087e3
Kor de Jong <k.dejong1@uu.nl>
2019-12-09 14:05:36 +0100

    Add build variant with support for APEX

--- rel_with_deb_info_build.sh
+++ rel_with_deb_info_build.sh
@@ -7,10 +7,9 @@ build_type=RelWithDebInfo
 source build_common.sh
 rebuild_project $build_type default ""
 rebuild_project $build_type papi "-DHPX_WITH_PAPI:BOOL=ON"
-# rebuild_project $build_type apex "
-#     -DHPX_WITH_APEX:BOOL=ON
-#     -DAPEX_WITH_OTF2:BOOL=ON
-# "
-# #   -DAPEX_WITH_PAPI:BOOL=ON
-# # HPX_WITH_APEX_TAG "v2.1.3"
-# # HPX_WITH_APEX_NO_UPDATE ON
+rebuild_project $build_type apex "
+    -DHPX_WITH_APEX:BOOL=ON
+    -DHPX_WITH_APEX_TAG:STRING=develop
+"
+# -DAPEX_WITH_OTF2:BOOL=ON
+# -DACTIVEHARMONY_ROOT=<path/to/otf2>

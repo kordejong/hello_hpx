a8cf20f69c461bf3a0c3cfde89d2e8771b1755fc
Kor de Jong <k.dejong1@uu.nl>
2019-12-16 14:26:47 +0100

    Add default build flags

--- build_common.sh
+++ build_common.sh
@@ -25,6 +25,10 @@ function build_project()
 
     tmp_root=${TMPDIR:-/tmp}/$basename/$build_type-$variant
     build_root=$tmp_root/build
+    default_cmake_flags="
+        -HPX_WITH_MALLOC:STRING=tcmalloc
+        -HPX_WITH_HWLOC:BOOL=ON
+    "
 
     mkdir -p `dirname $build_root`
     mkdir $build_root
@@ -32,6 +36,7 @@ function build_project()
         -DCMAKE_BUILD_TYPE=$build_type \
         -DHELLO_HPX_REPOSITORY_CACHE:PATH=$repository_cache \
         $cmake_flags \
+        $default_cmake_flags \
         -S $source_root \
         -B $build_root
     cmake --build $build_root
--- rel_with_deb_info_build.sh
+++ rel_with_deb_info_build.sh
@@ -9,7 +9,6 @@ rebuild_project $build_type default ""
 rebuild_project $build_type papi "-DHPX_WITH_PAPI:BOOL=ON"
 rebuild_project $build_type apex "
     -DHPX_WITH_APEX:BOOL=ON
-    -DHPX_WITH_APEX_TAG:STRING=develop
     -DAPEX_WITH_OTF2:BOOL=ON
     -DHELLO_HPX_BUILD_OTF2:BOOL=ON
     -DHPX_WITH_EXAMPLES:BOOL=ON
